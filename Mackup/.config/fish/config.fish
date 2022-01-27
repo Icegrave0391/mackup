043834453861c728479b418996db330fa6565120
Icegrave0391 <chuqiz@comp.nus.edu.sg>
2022-01-27 20:24:20 +0800

    config file added

--- Mackup/.config/fish/config.fish
+++ Mackup/.config/fish/config.fish
@@ -43,3 +43,9 @@ alias vi='nvim'
 alias vim='nvim'
 alias r='ranger'
 
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /usr/local/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
